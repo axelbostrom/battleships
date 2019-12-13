5077fb4baa66b8b6ea190a3c6de4a3680489ab19
axelbostrom <axel.bostrom103@gmail.com>
2019-12-13 11:17:44 +0100

    fixed wierd merge error

--- Game.java
+++ Game.java
@@ -2,18 +2,12 @@ package battleships;
 
 import java.util.*;
 
-<<<<<<< HEAD
 public class Game implements GameItem {
-	
 	protected String name;
-=======
-public class Game {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 	public static char t;
 	public static int p1Ships; //Player one ships
 	public static int p2Ships; //Player two ships
 	public static int c1Ships; //Computers ships
-<<<<<<< HEAD
 	public static String[] ships = new String[Constants.BATTLESHIP_SIZE];
 	public static String[][] grid = new String [Constants.rowSize][Constants.colSize];
 	static Scanner scan = new Scanner(System.in);
@@ -34,26 +28,6 @@ public class Game {
 	public String getName() {
         return name;
     }
-    
-	public void runGame() {
-		System.out.println("Welcome to Battle Ships!");
-		System.out.println();
-		//Launches all methods for game
-
-		//gridMap();
-		
-		//Add player
-		newPlayer();
-		
-		//Launch addShips
-=======
-	public static String[][] grid = new String [Constants.rowSize][Constants.colSize];
-	public static String[] ships = new String[Constants.BATTLESHIP_SIZE];
-	static Scanner scan = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		runGame();
-	}
 	
 	public static void runGame() {
 		System.out.println("Welcome to Battle Ships!");
@@ -63,20 +37,15 @@ public class Game {
 
 		//gridMap();
 		//Add player
-		player();
+		newPlayer();
 		
 		//Launch addShipss
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		addShips();
 		
 		attack();
 	}
-	
-<<<<<<< HEAD
-	public void gridMap() {
-=======
+
 	public static void gridMap() {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		System.out.print("   ");
 		for(int i = 0; i < Constants.rowSize; i++) {
 			System.out.print(i + "  ");
@@ -96,13 +65,9 @@ public class Game {
 			System.out.println();
 		}
 	}
-	
-<<<<<<< HEAD
-	public void addShips() {
-=======
+
 	public static void addShips() {
 		System.out.println();
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		p1Ships = 5;
 		c1Ships = 5;
 		
@@ -138,11 +103,7 @@ public class Game {
 		}
 	}
 	
-<<<<<<< HEAD
-	public String newPlayer() {
-=======
-	public static String player() {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
+	public static String newPlayer() {
 		String name = null;
 		int playernbr = 1;
 		int hitrate = 0;
@@ -159,11 +120,9 @@ public class Game {
 			System.out.println("Incorrect input, please only use number 1 or 2.");
 			System.out.println("Try again...");
 			System.out.println();
-<<<<<<< HEAD
+
 			newPlayer();
-=======
-			player();
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
+
 		}
 		for (int i = 0; i < choice; i++) {
 			if (choice == 2) {
@@ -182,29 +141,18 @@ public class Game {
 			} else {
 				System.out.println("Incorrect input... try again.");
 				System.out.println();
-<<<<<<< HEAD
 				newPlayer();
-=======
-				player();
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 			}
 		}
 		return null;
 	}
-	
-<<<<<<< HEAD
+
 	public void playerTurn() {
 		
 	}
-	
-	public void attack() {
-=======
-	public static void playerTurn() {
-		
-	}
+
 	
 	public static void attack() {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		System.out.println("Player 1: Get ready!");
 		System.out.println();
 		System.out.println("Please enter x coordinate to attack: ");
@@ -217,21 +165,13 @@ public class Game {
 		else
 			missedAttack(x, y);	
 	}
-	
-<<<<<<< HEAD
-	public void missedAttack(int x, int y) {
-=======
+
 	public static void missedAttack(int x, int y) {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		System.out.println("You missed!");
 		System.out.println("Player 2's turn.");
 	}
 	
-<<<<<<< HEAD
-	public void hitAttack(int x, int y) {
-=======
 	public static void hitAttack(int x, int y) {
->>>>>>> d63f6b4db8e8e2b0c25348a0ccf9af1249847edd
 		System.out.println("You hit a ship at grid: " + x + ", " + y);
 	}
 }
